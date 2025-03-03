aa33a7feedd5f4e66e423e83e904eceab17edc44
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-03 20:07:26 +0530

    Update url.js

--- src/Constant/url.js
+++ src/Constant/url.js
@@ -1,11 +1,4 @@
-export const adminBaseUrl = "https://sargalayamserver-production.up.railway.app/admin";
-export const teamBaseUrl = "https://sargalayamserver-production.up.railway.app/teams";
-export const imageUrl = "https://sargalayamserver-production.up.railway.app/teamImages";
-export const imageUrl2 = "https://sargalayamserver-production.up.railway.app/programImg";
-
-
-//render
-// export const adminBaseUrl = "https://sargalayamserver.onrender.com/admin"
-// export const teamBaseUrl = "https://sargalayamserver.onrender.com/teams";
-// export const imageUrl = "https://sargalayamserver.onrender.com/teamImages";
-// export const imageUrl2 = "https://sargalayamserver.onrender.com/programImg";
+export const adminBaseUrl = "https://sargalayamyygt.onrender.com/admin";
+export const teamBaseUrl = "https://sargalayamyygt.onrender.com/teams";
+ export const imageUrl = "https://sargalayamyygt.onrender.com/teamImages";
+export const imageUrl2 = "https://sargalayamyygt.onrender.com/programImg";
