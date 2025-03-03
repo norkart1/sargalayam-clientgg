eedf80a09fdbbfd1b409852fa36ec58e32258252
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-03 21:53:07 +0530

    Update url.js

--- src/Constant/url.js
+++ src/Constant/url.js
@@ -1,4 +1,4 @@
-export const adminBaseUrl = "https://sargalayamyygt.onrender.com/admin";
-export const teamBaseUrl = "https://sargalayamyygt.onrender.com/teams";
- export const imageUrl = "https://sargalayamyygt.onrender.com/teamImages";
-export const imageUrl2 = "https://sargalayamyygt.onrender.com/programImg";
+export const adminBaseUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/admin";
+export const teamBaseUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/teams";
+ export const imageUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/teamImages";
+export const imageUrl2 = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/programImg";
