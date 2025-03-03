9d81b2bcc69f1f5c92468bdfa34c9d768d77db17
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-03 21:58:18 +0530

    Update url.js

--- src/Constant/url.js
+++ src/Constant/url.js
@@ -1,4 +1,4 @@
-export const adminBaseUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/admin";
-export const teamBaseUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/teams";
- export const imageUrl = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/teamImages";
-export const imageUrl2 = "https://9000-idx-server-1741017136697.cluster-bec2e4635ng44w7ed22sa22hes.cloudworkstations.dev/?monospaceUid=772088&embedded=0/programImg";
+export const adminBaseUrl = "https://server-behw.onrender.com/admin";
+export const teamBaseUrl = "https://server-behw.onrender.com/teams";
+ export const imageUrl = "https://server-behw.onrender.com/teamImages";
+export const imageUrl2 = "https://server-behw.onrender.com/programImg";
